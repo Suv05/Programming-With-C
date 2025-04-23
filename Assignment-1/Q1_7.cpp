8c0c2d16f8c5e7cb0bf517ba4e9b7075512ca0d7
suvam <suvampravu@gmail.com>
2025-04-23 23:46:56 +0530

    commit changes

---
+++ Assignment-1/Q1_7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define MAX3(a, b, c) ((a > b && a > c) ? a : (b > c ? b : c))
+
+using namespace std;
+
+int main() {
+    int a = 10, b = 20, c = 15;
+    cout << "Largest is: " << MAX3(a, b, c) << endl;
+    return 0;
+}
